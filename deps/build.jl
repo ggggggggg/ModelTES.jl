425bf355b33331b0bb0a664b3e09f781f1d7980e
Joseph Fowler <drjoefowler@gmail.com>
2019-08-29 11:31:27 -0600

    Use our new NIST-specific Pkg registry
    This made testing work at Travis for https://github.com/usnistgov/Pope.jl

--- deps/build.jl
+++
@@ -1,8 +0,0 @@
-println("running ModelTES build.jl: "*@__FILE__)
-
-using Pkg
-println("Installing ARMA.jl")
-Pkg.add("https://github.com/joefowler/ARMA.jl")
-Pkg.activate(".")
-Pkg.build()
-Pkg.test()
